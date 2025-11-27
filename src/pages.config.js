a9b1682e46d696ec1e66979c73cce0ef49c2e984
Dat Nguyen <datnguyen@gmail.com>
2025-11-27 21:27:41 +0700

    update

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,12 +1,14 @@
 import Layout from './Layout.jsx';
 import AdminLayout from './pages/admin/AdminLayout.jsx';
 import Home from './pages/Home.jsx';
+import Dashboard from './pages/admin/Dashboard.jsx';
 
 export const PAGES = {
     Home,
 };
 
 export const ADMINS = {
+    Dashboard,
 };
 
 export const PRIVATE_PAGES = {
