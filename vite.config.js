506a5f3dd1eb9562068026af2b46ef55f23baccf
Charles Nguyen <dat.nguyen@metacrew.vn>
2026-01-06 16:45:38 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -72,6 +72,7 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+    dedupe: ['react', 'react-dom']
   },
   optimizeDeps: {
     include: ["react", "react-dom"],
