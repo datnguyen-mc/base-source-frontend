f9b570b4bbf8f4701750b6c29bf158c1491d9ec1
Charles Nguyen <dat.nguyen@metacrew.vn>
2026-03-08 09:57:08 +0700

    udpate

--- src/api/entities.js
+++ src/api/entities.js
@@ -1,6 +1,7 @@
 import { vibex } from './vibexClient';
 
 export const Query = vibex.entities.Query;
+export const Paging = vibex.entities.Query.paging;
 
 // auth sdk:
 export const User = vibex.auth;
